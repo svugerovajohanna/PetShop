3f4da3fb170357ecc74bc8565931bd012852c96d
svugerovajohanna <svuj00@vse.cz>
2019-05-26 21:53:33 +0200

    database config and basic operations

---
+++ database/db-config.php
@@ -0,0 +1,10 @@
+<?php
+
+const DB_HOST = 'localhost';
+const DB_DATABASE = 'test';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = 'heslo';
+
+//const DB_USERNAME = 'svuj00';
+//const DB_PASSWORD = 'butsyb-voMvav-2bocny';
+?>
